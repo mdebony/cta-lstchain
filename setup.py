e2dbeb4fc359ae3736fa9308e72d05dc6b841e4d
Mathieu de Bony <lavergne@lapp.in2p3.fr>
2022-01-21 14:51:27 +0100

    Update depedency pyirf : 0.5 -> 0.6 gammapy : 0.18 -> 0.19

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'ctapipe_io_lst~=0.13.2',
         'ctaplot~=0.5.5',
         'eventio>=1.5.1,<2.0.0a0',  # at least 1.1.1, but not 2
-        'gammapy~=0.18.2',
+        'gammapy~=0.19',
         'h5py',
         'joblib',
         'matplotlib~=3.4.3',  # 3.5 is incompatible with gammapy 0.18.2
@@ -53,7 +53,7 @@ setup(
         'numba',
         'numpy<1.22.0a0',
         'pandas',
-        'pyirf~=0.5.0',
+        'pyirf~=0.6.0',
         'scipy',
         'seaborn',
         'scikit-learn~=1.0',
